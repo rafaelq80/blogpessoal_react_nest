0109a536254261e645c5283f208708264f8dad77
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2022-08-11 01:40:58 -0300

    Update Service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://bpnest2022.herokuapp.com'
+    baseURL: 'https://bpnest.herokuapp.com'
     //baseURL: 'http://localhost:4000'
 })
 
